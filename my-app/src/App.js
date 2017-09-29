50df6fcb8d74c56dc3206160a5b03634b0e82f59
ashish mundada <amundada04@gmail.com>
2017-09-29 11:45:31 -0500

    myapp

---
+++ my-app/src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Ex1 from './Ex1.js';
+
+class App extends Component {
+  render() {
+    return (
+      
+     /* <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">MUNDADA</h1>
+        </header>
+        <p className="App-intro">
+          Hi! Ashish Mundada, edit <code>src/App.js</code> and save to reload.
+        </p>*/
+        <div>
+          <p>how</p>
+        <Ex1 />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ my-app/src/Ex1.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Ex1 extends Component{
+    render(){
+        return <h1>ashish how r u</h1>;
+    }
+}
+
+export default Ex1;
